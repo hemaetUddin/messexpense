896f4fb240dcd665bf9222e2d649f0ab195171bf
Rinto <rinto.sp@gmail.com>
2015-02-25 14:49:46 -0800

    Login panel addes

---
+++ me_loggedin.php
@@ -0,0 +1,24 @@
+<?php
+include'common/config.php';
+$user = $_POST['uname'];
+$pass = $_POST['password'];
+
+$myquery = "SELECT * FROM users WHERE username='".$user ."' AND password ='".$pass."' LIMIT 1";
+$result = $db->query($myquery);
+// while (list($id,$name,$pass)=$result->fetch_row()) {
+// 	echo $id;
+// }
+
+
+$num_rows = $result->num_rows;
+if($num_rows!=0){
+	echo "You have successfully loggedin";
+	// exit();
+}else{
+	header('Location: login.php');
+	echo "Invalid Credentials";
+}
+
+include'index.php';
+
+?>
